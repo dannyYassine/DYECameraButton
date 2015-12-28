105b16494ea8b8d7dea661e856e680ff8efe5c59
Danny Yassine <dannyyassine@gmail.com>
2015-12-28 10:52:01 -0500

    Initial Commit

---
+++ DYECameraButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYECameraButton
+//
+//  Created by Danny Yassine on 2015-12-28.
+//  Copyright © 2015 Danny Yassine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
